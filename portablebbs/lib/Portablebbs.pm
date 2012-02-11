74917a54e741e2a163698d5cd05b1fc978fe2975
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-02-11 13:54:22 +0900

    added application template

---
+++ portablebbs/lib/Portablebbs.pm
@@ -0,0 +1,18 @@
+package Portablebbs;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+
+  # Routes
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->route('/')->to('example#welcome');
+}
+
+1;
